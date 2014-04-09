8a193b1a8e223407acc01f45d9e1d75f005bb39b
Daniel Lowrey <rdlowrey@php.net>
2014-04-09 10:32:07 -0400

    Future is now an interface

--- lib/Future.php
+++ lib/Future.php
@@ -5,11 +5,7 @@ namespace Alert;
 /**
  * A "placeholder" value that will be resolved at some point in the future.
  */
-class Future {
-    private $onComplete = [];
-    private $isComplete = FALSE;
-    private $value;
-    private $error;
+interface Future {
 
     /**
      * Pass the Future to the specified callback upon completion regardless of success or failure
@@ -17,24 +13,14 @@ class Future {
      * @param callable $onComplete
      * @return Future Returns the current object instance
      */
-    public function onComplete(callable $onComplete) {
-        if ($this->isComplete) {
-            call_user_func($onComplete, $this);
-        } else {
-            $this->onComplete[] = $onComplete;
-        }
-
-        return $this;
-    }
+    public function onComplete(callable $onComplete);
 
     /**
      * Has the Future completed (succeeded/failure is irrelevant)?
      *
      * @return bool
      */
-    public function isComplete() {
-        return $this->isComplete;
-    }
+    public function isComplete();
 
     /**
      * Has the Future value been successfully resolved?
@@ -42,15 +28,7 @@ class Future {
      * @throws \LogicException If the Future is still pending
      * @return bool
      */
-    public function succeeded() {
-        if ($this->isComplete) {
-            return empty($this->error);
-        } else {
-            throw new \LogicException(
-                'Cannot retrieve success status: Future still pending'
-            );
-        }
-    }
+    public function succeeded();
 
     /**
      * Retrieve the value that successfully fulfilled the Future
@@ -59,17 +37,7 @@ class Future {
      * @throws \Exception If the Future failed the exception that caused the failure is thrown
      * @return mixed
      */
-    public function getValue() {
-        if (!$this->isComplete) {
-            throw new \LogicException(
-                'Cannot retrieve value: Future still pending'
-            );
-        } elseif ($this->error) {
-            throw $this->error;
-        } else {
-            return $this->value;
-        }
-    }
+    public function getValue();
 
     /**
      * Retrieve the Exception responsible for Future resolution failure
@@ -77,31 +45,5 @@ class Future {
      * @throws \LogicException If the Future succeeded or is still pending
      * @return \Exception
      */
-    public function getError() {
-        if ($this->isComplete) {
-            return $this->error;
-        } else {
-            throw new \LogicException(
-                'Cannot retrieve error: Future still pending'
-            );
-        }
-    }
-
-    private function resolve(\Exception $error = NULL, $value = NULL) {
-        if ($this->isComplete) {
-            throw new \LogicException(
-                'Cannot succeed: Future already resolved'
-            );
-        }
-
-        $this->isComplete = TRUE;
-        $this->error = $error;
-        $this->value = $value;
-
-        if ($this->onComplete) {
-            foreach ($this->onComplete as $onComplete) {
-                call_user_func($onComplete, $this);
-            }
-        }
-    }
+    public function getError();
 }
