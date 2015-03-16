018564e47bb2d743bb8ba22abe71affca168697b
Daniel Lowrey <rdlowrey@php.net>
2015-03-16 15:04:01 -0400

    Update tests

--- test/PrivateFutureTest.php
+++ test/PrivateFutureTest.php
@@ -3,95 +3,9 @@
 namespace Amp\Test;
 
 use Amp\PrivateFuture;
-use Amp\NativeReactor;
 
-class PrivateFutureTest extends \PHPUnit_Framework_TestCase {
-    public function testPromiseReturnsUnresolvedInstance() {
-        $promisor = new PrivateFuture;
-        $this->assertInstanceOf('Amp\Unresolved', $promisor->promise());
-    }
-
-    /**
-     * @expectedException \LogicException
-     * @expectedExceptionMessage Promise already resolved
-     */
-    public function testSucceedThrowsIfAlreadyResolved() {
-        $promisor = new PrivateFuture;
-        $promisor->succeed(42);
-        $promisor->succeed('zanzibar');
-    }
-
-    /**
-     * @expectedException \LogicException
-     * @expectedExceptionMessage A Promise cannot act as its own resolution result
-     */
-    public function testSucceedThrowsIfPromiseIsTheResolutionValue() {
-        $promisor = new PrivateFuture;
-        $promise = $promisor->promise();
-        $promisor->succeed($promise);
-    }
-
-    /**
-     * @expectedException \LogicException
-     * @expectedExceptionMessage Promise already resolved
-     */
-    public function testFailThrowsIfAlreadyResolved() {
-        $promisor = new PrivateFuture;
-        $promisor->succeed(42);
-        $promisor->fail(new \Exception);
-    }
-
-    public function testSucceedingWithPromisePipelinesResult() {
-        (new NativeReactor)->run(function($reactor) {
-            $promisor = new PrivateFuture;
-            $next = new PrivateFuture;
-            $reactor->once(function() use ($next) {
-                $next->succeed(42);
-            }, $msDelay = 1);
-            $promisor->succeed($next->promise());
-            $result = (yield $promisor->promise());
-            $this->assertSame(42, $result);
-        });
-    }
-
-    /**
-     * @expectedException \RuntimeException
-     * @expectedExceptionMessage fugazi
-     */
-    public function testFailingWithPromisePipelinesResult() {
-        (new NativeReactor)->run(function($reactor) {
-            $promisor = new PrivateFuture;
-            $next = new PrivateFuture;
-
-            $reactor->once(function() use ($next) {
-                $next->fail(new \RuntimeException('fugazi'));
-            }, $msDelay = 10);
-
-            $promisor->succeed($next->promise());
-
-            yield $promisor->promise();
-        });
-    }
-
-    public function testUpdate() {
-        $updatable = 0;
-        (new NativeReactor)->run(function() use (&$updatable) {
-            $i = 0;
-            $promisor = new PrivateFuture;
-            $updater = function($reactor, $watcherId) use ($promisor, &$i) {
-                $promisor->update(++$i);
-                if ($i === 3) {
-                    $reactor->cancel($watcherId);
-                    // reactor run loop should now be able to exit
-                }
-            };
-            $promise = $promisor->promise();
-            $promise->watch(function($updateData) use (&$updatable) {
-                $updatable += $updateData;
-            });
-            yield 'repeat' => [$updater, $msDelay = 10];
-        });
-
-        $this->assertSame(6, $updatable);
+class PrivateFutureTest extends PromisorTest {
+    protected function getPromisor() {
+        return new PrivateFuture;
     }
 }
---
+++ test/PromisorTest.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace Amp\Test;
+
+use Amp\NativeReactor;
+
+abstract class PromisorTest extends \PHPUnit_Framework_TestCase {
+    abstract protected function getPromisor();
+
+    public function testWhenInvokesCallbackWithResultIfAlreadySucceeded() {
+        $invoked = 0;
+        $promisor = $this->getPromisor();
+        $promise = $promisor->promise();
+        $promisor->succeed(42);
+        $promise->when(function($e, $r) use (&$invoked) {
+            $this->assertSame(42, $r);
+            $this->assertNull($e);
+            ++$invoked;
+        });
+    }
+
+    public function testWhenInvokesCallbackWithErrorIfAlreadyFailed() {
+        $promisor = $this->getPromisor();
+        $promise = $promisor->promise();
+        $exception = new \Exception('test');
+        $promisor->fail($exception);
+        $promise->when(function($e, $r) use ($exception) {
+            $this->assertSame($exception, $e);
+            $this->assertNull($r);
+        });
+    }
+
+    /**
+     * @expectedException \LogicException
+     * @expectedExceptionMessage Promise already resolved
+     */
+    public function testSucceedThrowsIfAlreadyResolved() {
+        $promisor = $this->getPromisor();
+        $promisor->succeed(42);
+        $promisor->succeed('zanzibar');
+    }
+
+    /**
+     * @expectedException \LogicException
+     * @expectedExceptionMessage A Promise cannot act as its own resolution result
+     */
+    public function testSucceedThrowsIfPromiseIsTheResolutionValue() {
+        $promisor = $this->getPromisor();
+        $promise = $promisor->promise();
+        $promisor->succeed($promise);
+    }
+
+    /**
+     * @expectedException \LogicException
+     * @expectedExceptionMessage Promise already resolved
+     */
+    public function testFailThrowsIfAlreadyResolved() {
+        $promisor = $this->getPromisor();
+        $promisor->succeed(42);
+        $promisor->fail(new \Exception);
+    }
+
+    public function testSucceedingWithPromisePipelinesResult() {
+        (new NativeReactor)->run(function($reactor) {
+            $next = $this->getPromisor();
+            $promisor = $this->getPromisor();
+            $promisor->succeed($next->promise());
+            $once = function() use ($next) { $next->succeed(42); };
+            $reactor->once($once, $msDelay = 10);
+            yield;
+            $result = yield $promisor->promise();
+            $this->assertSame(42, $result);
+        });
+    }
+
+    /**
+     * @expectedException \RuntimeException
+     * @expectedExceptionMessage fugazi
+     */
+    public function testFailingWithPromisePipelinesResult() {
+        (new NativeReactor)->run(function($reactor) {
+            $promisor = $this->getPromisor();
+            $next = $this->getPromisor();
+            $once = function() use ($next) { $next->fail(new \RuntimeException('fugazi')); };
+            $reactor->once($once, $msDelay = 10);
+            yield;
+            $promisor->succeed($next->promise());
+
+            yield $promisor->promise();
+        });
+    }
+
+    public function testUpdate() {
+        $updatable = 0;
+        (new NativeReactor)->run(function($reactor) use (&$updatable) {
+            $i = 0;
+            $promisor = $this->getPromisor();
+            $updater = function($reactor, $watcherId) use ($promisor, &$i) {
+                $promisor->update(++$i);
+                if ($i === 3) {
+                    $reactor->cancel($watcherId);
+                    // reactor run loop should now be able to exit
+                }
+            };
+            $promise = $promisor->promise();
+
+            $promise->watch(function($updateData) use (&$updatable) {
+                $updatable += $updateData;
+            });
+            $reactor->repeat($updater, $msDelay = 10);
+        });
+
+        $this->assertSame(6, $updatable);
+    }
+}
