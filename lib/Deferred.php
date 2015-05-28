31f21da9e93231ab16fa7656113b1ee1ac10b9b2
Daniel Lowrey <rdlowrey@php.net>
2015-05-27 22:03:08 -0400

    Improve Deferred performance switching

--- lib/Deferred.php
+++ lib/Deferred.php
@@ -2,8 +2,15 @@
 
 namespace Amp;
 
-if (defined("AMP_PRODUCTION_MODE") && AMP_PRODUCTION_MODE) {
-    final class Deferred implements Promisor, Promise { use PublicPromisor; }
-} else {
-    final class Deferred implements Promisor { use PrivatePromisor; }
+try {
+    if (@assert(false)) {
+        // PHP7 production environment (zend.assertions=0, assert.exception=0)
+        eval("namespace Amp; final class Deferred implements Promisor, Promise { use PublicPromisor; }");
+    } else {
+        // PHP < 7 or dev environment (zend.assertions=1, assert.exception=0)
+        final class Deferred implements Promisor { use PrivatePromisor; }
+    }
+} catch (\AssertionException $e) {
+    // PHP7 dev environment (zend.assertions=1, assert.exception=1)
+    eval("namespace Amp; final class Deferred implements Promisor { use PrivatePromisor; }");
 }
