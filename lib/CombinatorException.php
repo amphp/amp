ab8c2a06d6a5cdd1656956691daea2b7434f20f8
Niklas Keller <me@kelunik.com>
2016-02-25 12:50:51 +0100

    Expose combined exceptions in combinator exception

--- lib/CombinatorException.php
+++ lib/CombinatorException.php
@@ -2,4 +2,15 @@
 
 namespace Amp;
 
-class CombinatorException extends \RuntimeException {}
+class CombinatorException extends \RuntimeException {
+	private $combinedExceptions;
+
+	public function __construct($message, array $combinedExceptions = []) {
+		parent::__construct($message, 0, null);
+		$this->combinedExceptions = $combinedExceptions;
+	}
+
+	public function getCombinedExceptions() {
+		return $this->combinedExceptions;
+	}
+}
