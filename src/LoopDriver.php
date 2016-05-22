c98af47036dd8cf7bfa19e5b225f70a1d8bb797d
Cees-Jan Kiewiet <ceesjank@gmail.com>
2016-05-22 19:36:13 +0200

    s/non/none

--- src/LoopDriver.php
+++ src/LoopDriver.php
@@ -9,7 +9,7 @@ interface LoopDriver
      *
      * The loop MUST continue to run until it is either stopped explicitly, no referenced watchers exist anymore, or an
      * exception is thrown that cannot be handled. Exceptions that cannot be handled are exceptions thrown from an
-     * error handler or exceptions that would be passed to an error handler but non exists to handle them.
+     * error handler or exceptions that would be passed to an error handler but none exists to handle them.
      *
      * @return void
      */
