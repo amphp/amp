4908813e0897786772d22327b6315f033b7f3967
Aaron Piotrowski <aaron@trowski.com>
2016-05-27 10:45:45 -0500

    Refactor Registry for Drivers

--- src/Loop/Driver.php
+++ src/Loop/Driver.php
@@ -149,6 +149,31 @@ interface Driver
      * @return void
      */
     public function unreference($watcherId);
+    
+    /**
+     * Stores information in the loop bound registry. This can be used to store loop bound information. Stored
+     * information is package private. Packages MUST NOT retrieve the stored state of other packages.
+     *
+     * Therefore packages SHOULD use the following prefix to keys: `vendor.package.`
+     *
+     * @param string $key namespaced storage key
+     * @param mixed $value the value to be stored
+     *
+     * @return void
+     */
+    public function storeState($key, $value);
+
+    /**
+     * Fetches information stored bound to the loop. Stored information is package private. Packages MUST NOT retrieve
+     * the stored state of other packages.
+     *
+     * Therefore packages SHOULD use the following prefix to keys: `vendor.package.`
+     *
+     * @param string $key namespaced storage key
+     *
+     * @return mixed previously stored value or null if it doesn't exist
+     */
+    public function fetchState($key);
 
     /**
      * Set a callback to be executed when an error occurs.
--- src/Loop/Registry.php
+++ src/Loop/Registry.php
@@ -3,62 +3,47 @@
 namespace Interop\Async\Loop;
 
 /**
- * State registry to be used in Interop\Async\Loop.
- *
- * THIS TRAIT SHOULD NOT BE USED BY LOOP DRIVERS. It's the responsibility of the
- * loop accessor to manage this state.
+ * State registry to be used by classes implementing the Driver interface.
  */
 trait Registry
 {
     /**
      * @var array
      */
-    private static $registry = null;
+    private $registry = [];
 
     /**
-     * Stores information in the loop bound registry. This can be used to store
-     * loop bound information. Stored information is package private.
-     * Packages MUST NOT retrieve the stored state of other packages.
+     * Stores information in the loop bound registry. This can be used to store loop bound information. Stored
+     * information is package private. Packages MUST NOT retrieve the stored state of other packages.
      *
-     * Therefore packages SHOULD use the following prefix to keys:
-     * `vendor.package.`
+     * Therefore packages SHOULD use the following prefix to keys: `vendor.package.`
      *
      * @param string $key namespaced storage key
      * @param mixed $value the value to be stored
      *
      * @return void
      */
-    public static function storeState($key, $value)
+    public function storeState($key, $value)
     {
-        if (self::$registry === null) {
-            throw new \RuntimeException('Not within the scope of an event loop driver');
-        }
-
         if ($value === null) {
-            unset(self::$registry[$key]);
+            unset($this->registry[$key]);
         } else {
-            self::$registry[$key] = $value;
+            $this->registry[$key] = $value;
         }
     }
 
     /**
-     * Fetches information stored bound to the loop. Stored information is
-     * package private. Packages MUST NOT retrieve the stored state of
-     * other packages.
+     * Fetches information stored bound to the loop. Stored information is package private. Packages MUST NOT retrieve
+     * the stored state of other packages.
      *
-     * Therefore packages SHOULD use the following prefix to keys:
-     * `vendor.package.`
+     * Therefore packages SHOULD use the following prefix to keys: `vendor.package.`
      *
      * @param string $key namespaced storage key
      *
      * @return mixed previously stored value or null if it doesn't exist
      */
-    public static function fetchState($key)
+    public function fetchState($key)
     {
-        if (self::$registry === null) {
-            throw new \RuntimeException('Not within the scope of an event loop driver');
-        }
-
-        return isset(self::$registry[$key]) ? self::$registry[$key] : null;
+        return isset($this->registry[$key]) ? $this->registry[$key] : null;
     }
 }
