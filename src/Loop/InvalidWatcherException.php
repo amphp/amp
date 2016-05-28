f6c6f41adf3459220e2c3659095604bc319e7c3a
Bob Weinand <bobwei9@hotmail.com>
2016-05-28 15:57:07 +0200

    (Re-)Introduce InvalidWatcherException
    Fixes #58

---
+++ src/Loop/InvalidWatcherException.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Interop\Async\Loop;
+
+/**
+ * MUST be thrown if any operation (except cancel()) is attempted with an invalid watcher identifier. [Invalid watcher identifier: any identifier not yet emitted by the driver or cancelled by the user]
+ */
+class InvalidWatcherException extends \LogicException
+{
+
+}
