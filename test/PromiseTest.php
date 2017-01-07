bc110c4d639a9f7e96745aabbad95bc8330cb550
Niklas Keller <me@kelunik.com>
2017-01-07 14:24:21 +0100

    Add test for callback combination in WhenQueue

--- test/PromiseTest.php
+++ test/PromiseTest.php
@@ -2,7 +2,7 @@
 
 namespace Amp\Test;
 
-class Promise {
+class Promise implements \AsyncInterop\Promise {
     use \Amp\Internal\Placeholder {
         resolve as public;
         fail as public;
@@ -17,5 +17,38 @@ class PromiseTest extends \AsyncInterop\Promise\Test {
             [$promise, 'resolve'],
             [$promise, 'fail'],
         ];
+    }
+
+    public function testWhenQueueUnrolling() {
+        $count = 50;
+        $invoked = false;
+
+        $promise = new Promise;
+        $promise->when(function () { });
+        $promise->when(function () { });
+        $promise->when(function () use (&$invoked) {
+            $invoked = true;
+            $this->assertLessThan(30, count(debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS)));
+        });
+
+        $last = $promise;
+
+        $f = function () use (&$f, &$count, &$last) {
+            $p = new Promise;
+            $p->when(function () { });
+            $p->when(function () { });
+
+            $last->resolve($p);
+            $last = $p;
+
+            if (--$count > 0) {
+                $f();
+            }
+        };
+
+        $f();
+        $last->resolve();
+
+        $this->assertTrue($invoked);
     }
 }
