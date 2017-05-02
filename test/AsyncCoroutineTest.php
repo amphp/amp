1419bd6c0c94aaa46634284dac8b254847dc6641
Niklas Keller <me@kelunik.com>
2017-05-02 19:04:25 +0200

    Remove unused imports

--- test/AsyncCoroutineTest.php
+++ test/AsyncCoroutineTest.php
@@ -3,13 +3,9 @@
 namespace Amp\Test;
 
 use Amp;
-use Amp\Coroutine;
 use Amp\Failure;
 use Amp\Loop;
-use Amp\Promise;
-use Amp\Success;
 use PHPUnit\Framework\TestCase;
-use React\Promise\FulfilledPromise as FulfilledReactPromise;
 
 class AsyncCoroutineTest extends TestCase {
     public function testWithFailure() {
