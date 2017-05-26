bcb3e74907b4d249cf842d640efd46cf1247d257
Niklas Keller <me@kelunik.com>
2017-05-26 12:29:34 +0200

    Do not use verbose extraction output when installing UV

--- travis/install-uv.sh
+++ travis/install-uv.sh
@@ -4,8 +4,8 @@ wget https://github.com/libuv/libuv/archive/v1.x.tar.gz -O /tmp/libuv.tar.gz -q 
 wget https://github.com/bwoebi/php-uv/archive/master.tar.gz -O /tmp/php-uv.tar.gz -q &
 wait
 
-mkdir libuv && tar -xvf /tmp/libuv.tar.gz -C libuv --strip-components=1
-mkdir php-uv && tar -xvf /tmp/php-uv.tar.gz -C php-uv --strip-components=1
+mkdir libuv && tar -xf /tmp/libuv.tar.gz -C libuv --strip-components=1
+mkdir php-uv && tar -xf /tmp/php-uv.tar.gz -C php-uv --strip-components=1
 
 pushd libuv;
 ./autogen.sh
