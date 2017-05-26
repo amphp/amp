5f70ca1fe11d7ca80b21cb0ac9a83b9b60f864d3
Niklas Keller <me@kelunik.com>
2017-05-26 12:09:55 +0200

    Use dist files instead of git clones for UV

--- travis/install-uv.sh
+++ travis/install-uv.sh
@@ -1,18 +1,25 @@
 #!/usr/bin/env bash
 
-git clone https://github.com/libuv/libuv;
+mkdir libuv
+wget https://github.com/libuv/libuv/archive/v1.x.tar.gz -O /tmp/libuv.tar.gz
+tar -xvf /tmp/libuv.tar.gz -C libuv --strip-components=1
+
 pushd libuv;
-git checkout $(git describe --tags);
-./autogen.sh;
-./configure --prefix=$(dirname `pwd`)/libuv-install;
-make;
-make install;
-popd;
-git clone https://github.com/bwoebi/php-uv.git;
-pushd php-uv;
-phpize;
-./configure --with-uv=$(dirname `pwd`)/libuv-install;
-make;
-make install;
-popd;
-echo "extension=uv.so" >> "$(php -r 'echo php_ini_loaded_file();')";
+./autogen.sh
+./configure --prefix=$(dirname `pwd`)/libuv-install
+make
+make install
+popd
+
+mkdir php-uv
+wget https://github.com/bwoebi/php-uv/archive/master.tar.gz -O /tmp/php-uv.tar.gz
+tar -xvf /tmp/php-uv.tar.gz -C php-uv --strip-components=1
+
+pushd php-uv
+phpize
+./configure --with-uv=$(dirname `pwd`)/libuv-install
+make
+make install
+popd
+
+echo "extension=uv.so" >> "$(php -r 'echo php_ini_loaded_file();')"
