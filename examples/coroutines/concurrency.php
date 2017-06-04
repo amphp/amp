31f021e59a2112d98637d47ce1448ba3c4adf5bf
Niklas Keller <me@kelunik.com>
2017-06-04 16:12:46 +0200

    Add coroutine concurrency example

---
+++ examples/coroutines/concurrency.php
@@ -0,0 +1,33 @@
+<?php
+#!/usr/bin/env php
+
+require __DIR__ . '/../../vendor/autoload.php';
+
+use Amp\Delayed;
+use Amp\Loop;
+use function Amp\asyncCall;
+
+// Shows how two for loops are executed concurrently.
+
+// Note that the first two items are printed _before_ the Loop::run()
+// as they're executed immediately and do not register any timers or defers.
+
+asyncCall(function () {
+    for ($i = 0; $i < 5; $i++) {
+        print "1 - " . $i . PHP_EOL;
+        yield new Delayed(1000);
+    }
+});
+
+asyncCall(function () {
+    for ($i = 0; $i < 5; $i++) {
+        print "2 - " . $i . PHP_EOL;
+        yield new Delayed(400);
+    }
+});
+
+print "-- before Loop::run()" . PHP_EOL;
+
+Loop::run();
+
+print "-- after Loop::run()" . PHP_EOL;
