be3ae73a451e933d584ce9d9906776a11450c87a
Aaron Piotrowski <aaron@trowski.com>
2017-12-02 19:07:46 -0600

    Drop multiple definitions of Deferred and Emitter
    Closes #144, #157, #173, #189, #190.

--- lib/Deferred.php
+++ lib/Deferred.php
@@ -2,73 +2,46 @@
 
 namespace Amp;
 
-// @codeCoverageIgnoreStart
-try {
-    if (!@\assert(false)) {
-        development: // PHP 7 development (zend.assertions=1)
-        /**
-         * Deferred is a container for a promise that is resolved using the resolve() and fail() methods of this object.
-         * The contained promise may be accessed using the promise() method. This object should not be part of a public
-         * API, but used internally to create and resolve a promise.
-         */
-        final class Deferred {
-            /** @var \Amp\Promise */
-            private $promise;
+/**
+ * Deferred is a container for a promise that is resolved using the resolve() and fail() methods of this object.
+ * The contained promise may be accessed using the promise() method. This object should not be part of a public
+ * API, but used internally to create and resolve a promise.
+ */
+final class Deferred {
+    /** @var \Amp\Promise */
+    private $promise;
 
-            /** @var callable */
-            private $resolve;
-
-            /** @var callable */
-            private $fail;
-
-            public function __construct() {
-                $this->promise = new class($this->resolve, $this->fail) implements Promise {
-                    use CallableMaker, Internal\Placeholder;
-
-                    public function __construct(&$resolve, &$fail) {
-                        $resolve = $this->callableFromInstanceMethod("resolve");
-                        $fail = $this->callableFromInstanceMethod("fail");
-                    }
-                };
+    public function __construct() {
+        $this->promise = new class implements Promise {
+            use Internal\Placeholder {
+                resolve as public;
+                fail as public;
             }
-
-            /**
-             * @return \Amp\Promise
-             */
-            public function promise(): Promise {
-                return $this->promise;
-            }
-
-            /**
-             * Fulfill the promise with the given value.
-             *
-             * @param mixed $value
-             */
-            public function resolve($value = null) {
-                ($this->resolve)($value);
-            }
-
-            /**
-             * Fails the promise the the given reason.
-             *
-             * @param \Throwable $reason
-             */
-            public function fail(\Throwable $reason) {
-                ($this->fail)($reason);
-            }
-        }
-    } else {
-        production: // PHP 7 production environment (zend.assertions=0)
-        /**
-         * An optimized version of Deferred for production environments that is itself the promise. Eval is used to
-         * prevent IDEs and other tools from reporting multiple definitions.
-         */
-        eval('namespace Amp;
-        final class Deferred implements Promise {
-            use Internal\Placeholder { resolve as public; fail as public; }
-            public function promise(): Promise { return $this; }
-        }');
+        };
     }
-} catch (\AssertionError $exception) {
-    goto development; // zend.assertions=1 and assert.exception=1, use development definition.
-} // @codeCoverageIgnoreEnd
+
+    /**
+     * @return \Amp\Promise
+     */
+    public function promise(): Promise {
+        return $this->promise;
+    }
+
+    /**
+     * Fulfill the promise with the given value.
+     *
+     * @param mixed $value
+     */
+    public function resolve($value = null) {
+        $this->promise->resolve($value);
+    }
+
+    /**
+     * Fails the promise the the given reason.
+     *
+     * @param \Throwable $reason
+     */
+    public function fail(\Throwable $reason) {
+        $this->promise->fail($reason);
+    }
+}
