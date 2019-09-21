f5af933d8fbbed022e7bc41c0b04e20f65d8acdf
Niklas Keller <me@kelunik.com>
2019-09-21 21:54:05 +0200

    Improve scripting

--- travis/change-composer.php
+++ travis/change-composer.php
@@ -1,8 +1,30 @@
 <?php
 
-$file = __DIR__ . '/../composer.json';
-$data = \json_decode(file_get_contents($file));
+function modify(callable $callback)
+{
+    $file = __DIR__ . '/../composer.json';
+    $data = \json_decode(file_get_contents($file));
+    $callback($data);
+    \file_put_contents($file, \json_encode($data, \JSON_PRETTY_PRINT));
+}
 
-unset($data->config);
+$command = $argv[1];
 
-\file_put_contents($file, \json_encode($data, \JSON_PRETTY_PRINT));
+if ($command === 'drop-config') {
+    modify(static function ($config) {
+        unset($config->config);
+    });
+} else {
+    if ($command === 'configure-pcov') {
+        if (PHP_VERSION_ID >= 70100) {
+            shell_exec('composer require pcov/clobber ^1');
+
+            modify(static function ($config) {
+                $config->scripts = new \stdClass;
+                $config->scripts->{'post-autoload-dump'} = '\\pcov\\Clobber::autoload';
+            });
+
+            shell_exec('composer dump');
+        }
+    }
+}
