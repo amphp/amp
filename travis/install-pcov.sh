20f34eae2237dfd2d3810fa263160bb039632270
Niklas Keller <me@kelunik.com>
2019-09-21 21:32:23 +0200

    Install uopz

--- travis/install-pcov.sh
+++ travis/install-pcov.sh
@@ -4,6 +4,16 @@ if [[ ${TRAVIS_PHP_VERSION:0:3} == "7.0" ]]; then
     exit 0
 fi
 
+# required for pcov/clobber
+curl -LS https://pecl.php.net/get/uopz | tar -xz \
+ && pushd uopz-* \
+ && phpize \
+ && ./configure --enable-uopz \
+ && make \
+ && make install \
+ && popd \
+ && echo "extension=uopz.so" >> "$(php -r 'echo php_ini_loaded_file();')";
+
 curl -LS https://pecl.php.net/get/pcov | tar -xz \
  && pushd pcov-* \
  && phpize \
