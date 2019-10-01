51cc90970d28192bf31d0ebbb8c8902b6b7871f6
Niklas Keller <me@kelunik.com>
2019-10-01 19:28:37 +0200

    Revert coroutine changes

--- lib/Coroutine.php
+++ lib/Coroutine.php
@@ -2,7 +2,6 @@
 
 namespace Amp;
 
-use function Amp\Internal\formatStacktrace;
 use React\Promise\PromiseInterface as ReactPromise;
 
 /**
@@ -52,32 +51,16 @@ final class Coroutine implements Promise
         ));
     }
 
-    /** @var string Timeout watcher for each step. */
-    private $timeoutWatcher;
-
-    /** @var string Debug trace. */
-    private $trace;
-
     /**
      * @param \Generator $generator
      */
     public function __construct(\Generator $generator)
     {
-        $this->timeoutWatcher = Loop::delay(1000, function () {
-            $this->timeoutWatcher = Loop::delay(1000, function () {
-                fwrite(STDERR, $this->trace . "\r\n");
-            });
-
-            fwrite(STDERR, $this->trace . "\r\n");
-        });
-        $this->trace = formatStacktrace(\debug_backtrace(\DEBUG_BACKTRACE_IGNORE_ARGS));
-
         try {
             $yielded = $generator->current();
 
             if (!$yielded instanceof Promise) {
                 if (!$generator->valid()) {
-                    Loop::cancel($this->timeoutWatcher);
                     $this->resolve($generator->getReturn());
                     return;
                 }
@@ -85,7 +68,6 @@ final class Coroutine implements Promise
                 $yielded = self::transform($yielded, $generator);
             }
         } catch (\Throwable $exception) {
-            Loop::cancel($this->timeoutWatcher);
             $this->fail($exception);
             return;
         }
@@ -95,11 +77,6 @@ final class Coroutine implements Promise
          * @param mixed           $v Value to be sent into the generator.
          */
         $onResolve = function ($e, $v) use ($generator, &$onResolve) {
-            // Reset the timeout
-            Loop::disable($this->timeoutWatcher);
-            Loop::enable($this->timeoutWatcher);
-            $this->trace = formatStacktrace(\debug_backtrace(\DEBUG_BACKTRACE_IGNORE_ARGS));
-
             /** @var bool Used to control iterative coroutine continuation. */
             static $immediate = true;
 
@@ -130,7 +107,6 @@ final class Coroutine implements Promise
 
                         if (!$yielded instanceof Promise) {
                             if (!$generator->valid()) {
-                                Loop::cancel($this->timeoutWatcher);
                                 $this->resolve($generator->getReturn());
                                 $onResolve = null;
                                 return;
@@ -145,7 +121,6 @@ final class Coroutine implements Promise
 
                     $immediate = true;
                 } catch (\Throwable $exception) {
-                    Loop::cancel($this->timeoutWatcher);
                     $this->fail($exception);
                     $onResolve = null;
                 } finally {
