196eb14ed9fc8b9f1e3910c0c6a1f9b909763160
Niklas Keller <me@kelunik.com>
2019-10-01 20:06:20 +0200

    Add method to dump watchers keeping the loop running

--- lib/Loop/TracingDriver.php
+++ lib/Loop/TracingDriver.php
@@ -4,122 +4,187 @@ namespace Amp\Loop;
 
 use function Amp\Internal\formatStacktrace;
 
-class TracingDriver extends Driver {
+class TracingDriver extends Driver
+{
     private $driver;
+    private $enabledWatchers = [];
+    private $unreferencedWatchers = [];
     private $creationTraces = [];
     private $cancelTraces = [];
 
-    public function __construct(Driver $driver) {
+    public function __construct(Driver $driver)
+    {
         $this->driver = $driver;
     }
 
-    public function run() {
+    public function run()
+    {
         $this->driver->run();
     }
 
-    public function stop() {
+    public function stop()
+    {
         $this->driver->stop();
     }
 
-    public function defer(callable $callback, $data = null): string {
-        $id = $this->driver->defer($callback, $data);
+    public function defer(callable $callback, $data = null): string
+    {
+        $id = $this->driver->defer(function (...$args) use ($callback) {
+            $this->cancel($args[0]);
+            return $callback(...$args);
+        }, $data);
+
         $this->creationTraces[$id] = formatStacktrace(\debug_backtrace(\DEBUG_BACKTRACE_IGNORE_ARGS));
+        $this->enabledWatchers[$id] = true;
         return $id;
     }
 
-    public function delay(int $delay, callable $callback, $data = null): string {
-        $id = $this->driver->delay($delay, $callback, $data);
+    public function delay(int $delay, callable $callback, $data = null): string
+    {
+        $id = $this->driver->delay($delay, function (...$args) use ($callback) {
+            $this->cancel($args[0]);
+            return $callback(...$args);
+        }, $data);
+
         $this->creationTraces[$id] = formatStacktrace(\debug_backtrace(\DEBUG_BACKTRACE_IGNORE_ARGS));
+        $this->enabledWatchers[$id] = true;
         return $id;
     }
 
-    public function repeat(int $interval, callable $callback, $data = null): string {
+    public function repeat(int $interval, callable $callback, $data = null): string
+    {
         $id = $this->driver->repeat($interval, $callback, $data);
         $this->creationTraces[$id] = formatStacktrace(\debug_backtrace(\DEBUG_BACKTRACE_IGNORE_ARGS));
+        $this->enabledWatchers[$id] = true;
         return $id;
     }
 
-    public function onReadable($stream, callable $callback, $data = null): string {
+    public function onReadable($stream, callable $callback, $data = null): string
+    {
         $id = $this->driver->onReadable($stream, $callback, $data);
         $this->creationTraces[$id] = formatStacktrace(\debug_backtrace(\DEBUG_BACKTRACE_IGNORE_ARGS));
+        $this->enabledWatchers[$id] = true;
         return $id;
     }
 
-    public function onWritable($stream, callable $callback, $data = null): string {
+    public function onWritable($stream, callable $callback, $data = null): string
+    {
         $id = $this->driver->onWritable($stream, $callback, $data);
         $this->creationTraces[$id] = formatStacktrace(\debug_backtrace(\DEBUG_BACKTRACE_IGNORE_ARGS));
+        $this->enabledWatchers[$id] = true;
         return $id;
     }
 
-    public function onSignal(int $signo, callable $callback, $data = null): string {
+    public function onSignal(int $signo, callable $callback, $data = null): string
+    {
         $id = $this->driver->onSignal($signo, $callback, $data);
         $this->creationTraces[$id] = formatStacktrace(\debug_backtrace(\DEBUG_BACKTRACE_IGNORE_ARGS));
+        $this->enabledWatchers[$id] = true;
         return $id;
     }
 
-    public function enable(string $watcherId) {
+    public function enable(string $watcherId)
+    {
         try {
             $this->driver->enable($watcherId);
+            $this->enabledWatchers[$watcherId] = true;
         } catch (InvalidWatcherError $e) {
-            throw new InvalidWatcherError($watcherId, $e->getMessage() . "\r\n\r\n== Creation Trace =====\r\n" . $this->getCreationTrace($watcherId) . "\r\n\r\n== Cancel Trace =====\r\n" . $this->getCreationTrace($watcherId));
+            throw new InvalidWatcherError($watcherId,
+                $e->getMessage() . "\r\n\r\n== Creation Trace =====\r\n" . $this->getCreationTrace($watcherId) . "\r\n\r\n== Cancel Trace =====\r\n" . $this->getCreationTrace($watcherId));
         }
     }
 
-    public function cancel(string $watcherId) {
+    public function cancel(string $watcherId)
+    {
         $this->driver->cancel($watcherId);
         $this->creationTraces[$watcherId] = formatStacktrace(\debug_backtrace(\DEBUG_BACKTRACE_IGNORE_ARGS));
+        unset($this->enabledWatchers[$watcherId], $this->unreferencedWatchers[$watcherId]);
     }
 
-    public function disable(string $watcherId) {
+    public function disable(string $watcherId)
+    {
         $this->driver->disable($watcherId);
+        unset($this->enabledWatchers[$watcherId]);
     }
 
-    public function reference(string $watcherId) {
+    public function reference(string $watcherId)
+    {
         try {
             $this->driver->reference($watcherId);
+            unset($this->unreferencedWatchers[$watcherId]);
         } catch (InvalidWatcherError $e) {
-            throw new InvalidWatcherError($watcherId, $e->getMessage() . "\r\n\r\nCreation Trace: " . $this->getCreationTrace($watcherId));
+            throw new InvalidWatcherError($watcherId,
+                $e->getMessage() . "\r\n\r\nCreation Trace: " . $this->getCreationTrace($watcherId));
         }
     }
 
-    public function unreference(string $watcherId) {
+    public function unreference(string $watcherId)
+    {
         $this->driver->unreference($watcherId);
+        $this->unreferencedWatchers[$watcherId] = true;
     }
 
-    public function setErrorHandler(callable $callback = null) {
+    public function setErrorHandler(callable $callback = null)
+    {
         return $this->driver->setErrorHandler($callback);
     }
 
-
     /** @inheritdoc */
-    protected function activate(array $watchers) {
-        // nothing to do in a decorator
-    }
-
-    /** @inheritdoc */
-    protected function dispatch(bool $blocking) {
-        // nothing to do in a decorator
-    }
-
-    /** @inheritdoc */
-    protected function deactivate(Watcher $watcher) {
-        // nothing to do in a decorator
-    }
-
-    /** @inheritdoc */
-    public function getHandle() {
+    public function getHandle()
+    {
         $this->driver->getHandle();
     }
 
-    private function getCreationTrace(string $watcher): string {
-        return $this->creationTraces[$watcher] ?? (function () use ($watcher) {
-            throw new InvalidWatcherError($watcher, "An invalid watcher has been used: " . $watcher);
-        })();
+    public function dump(): string
+    {
+        $dump = "Enabled, referenced watchers keeping the loop running: ";
+
+        foreach ($this->enabledWatchers as $watcher => $_) {
+            if (isset($this->unreferencedWatchers[$watcher])) {
+                continue;
+            }
+
+            $dump .= "Watcher ID: " . $watcher . "\r\n";
+            $dump .= $this->getCreationTrace($watcher);
+            $dump .= "\r\n\r\n";
+        }
+
+        return \rtrim($dump);
     }
 
-    private function getCancelTrace(string $watcher): string {
-        return $this->cancelTraces[$watcher] ?? (function () use ($watcher) {
+    /** @inheritdoc */
+    protected function activate(array $watchers)
+    {
+        // nothing to do in a decorator
+    }
+
+    /** @inheritdoc */
+    protected function dispatch(bool $blocking)
+    {
+        // nothing to do in a decorator
+    }
+
+    /** @inheritdoc */
+    protected function deactivate(Watcher $watcher)
+    {
+        // nothing to do in a decorator
+    }
+
+    private function getCreationTrace(string $watcher): string
+    {
+        if (!isset($this->creationTraces[$watcher])) {
+            new InvalidWatcherError($watcher, "An invalid watcher has been used: " . $watcher);
+        }
+
+        return $this->creationTraces[$watcher];
+    }
+
+    private function getCancelTrace(string $watcher): string
+    {
+        if (!isset($this->cancelTraces[$watcher])) {
             throw new InvalidWatcherError($watcher, "An invalid watcher has been used: " . $watcher);
-        })();
+        }
+
+        return $this->cancelTraces[$watcher];
     }
 }
