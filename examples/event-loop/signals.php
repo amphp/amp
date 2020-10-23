f8925bf9b997471a2635c3319c16e8950634121f
duckboy81 <duckboy81@users.noreply.github.com>
2020-10-23 15:13:22 -0500

    Allow signal handling for Uv w/o 100% cpu usage
    Ref #329

--- examples/event-loop/signals.php
+++ examples/event-loop/signals.php
@@ -9,7 +9,20 @@ print "Press Ctrl+C to exit..." . PHP_EOL;
 
 Loop::onSignal(SIGINT, function () {
     print "Caught SIGINT, exiting..." . PHP_EOL;
+    
+    // Check for a Uv driver
+    if (Loop::get() instanceof Amp\Loop\UvDriver) {
+
+        // Stop the loop
+        Loop::stop();
+
+        // Cannot exit out of a UvDriver loop here, can only stop the loop
+        return;
+    }
+    
     exit(0);
 });
 
 Loop::run();
+
+exit(0);
