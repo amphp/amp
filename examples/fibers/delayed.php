870af820e5aef31fbd84a26445f5e65cda480890
Markus Staab <maggus.staab@googlemail.com>
2021-01-06 22:02:13 +0100

    Remove leftover (#337)

--- examples/fibers/delayed.php
+++ examples/fibers/delayed.php
@@ -5,6 +5,5 @@ require __DIR__ . '/../../vendor/autoload.php';
 use Amp\Delayed;
 use function Amp\await;
 
-// Note that the closure declares void as a return type, not Promise or Generator.
 $result = await(new Delayed(1000, 42));
 \var_dump($result);
