c5a4ae5675b8b704f1766ba1a11397ba8a983fe1
Niklas Keller <me@kelunik.com>
2021-01-09 23:20:58 +0100

    Ignore only stream_select errors due to signal interruptions
    This also fixes signal interruptions not being ignored on PHP 8, as the error message changed slightly.
    
    Fixes #265.

--- test/Loop/NativeDriverTest.php
+++ test/Loop/NativeDriverTest.php
@@ -19,6 +19,66 @@ class NativeDriverTest extends DriverTest
         $this->assertNull($this->loop->getHandle());
     }
 
+    public function testTooLargeFileDescriptorSet()
+    {
+        $sockets = [];
+        $domain = \stripos(PHP_OS, 'win') === 0 ? STREAM_PF_INET : STREAM_PF_UNIX;
+
+        for ($i = 0; $i < 1001; $i++) {
+            $sockets[] = \stream_socket_pair($domain, STREAM_SOCK_STREAM, STREAM_IPPROTO_IP);
+        }
+
+        $this->expectException(\Exception::class);
+        $this->expectExceptionMessage("You have reached the limits of stream_select(). It has a FD_SETSIZE of 1024, but you have file descriptors numbered at least as high as 2006.You can install one of the extensions listed on https://amphp.org/amp/event-loop/#implementations to support a higher number of concurrent file descriptors. If a large number of open file descriptors is unexpected, you might be leaking file descriptors that aren't closed correctly.");
+
+        $this->start(function (Driver $loop) use ($sockets) {
+            $loop->delay(100, function () {
+                // here to provide timeout to stream_select, as the warning is only issued after the system call returns
+            });
+
+            foreach ($sockets as [$left, $right]) {
+                $loop->onReadable($left, function () {
+                    // nothing
+                });
+
+                $loop->onReadable($right, function () {
+                    // nothing
+                });
+            }
+        });
+    }
+
+    public function testSignalDuringStreamSelectIgnored()
+    {
+        $domain = \stripos(PHP_OS, 'win') === 0 ? STREAM_PF_INET : STREAM_PF_UNIX;
+        $sockets = \stream_socket_pair($domain, STREAM_SOCK_STREAM, STREAM_IPPROTO_IP);
+
+        $this->start(function (Driver $loop) use ($sockets) {
+            $socketWatchers = [
+                $loop->onReadable($sockets[0], function () {
+                    // nothing
+                }),
+                $loop->onReadable($sockets[1], function () {
+                    // nothing
+                }),
+            ];
+
+            $loop->onSignal(\SIGUSR2, function ($signalWatcher) use ($socketWatchers, $loop) {
+                $loop->cancel($signalWatcher);
+
+                foreach ($socketWatchers as $watcher) {
+                    $loop->cancel($watcher);
+                }
+
+                $this->assertTrue(true);
+            });
+
+            $loop->delay(100, function () {
+                \proc_open('sh -c "sleep 1; kill -12 ' . \getmypid() . '"', [], $pipes);
+            });
+        });
+    }
+
     /**
      * @requires PHP 7.1
      */
@@ -28,7 +88,7 @@ class NativeDriverTest extends DriverTest
 
         try {
             $this->start(function (Driver $loop) use (&$invoked) {
-                $watcher = $loop->onSignal(SIGUSR1, function () use (&$invoked) {
+                $watcher = $loop->onSignal(\SIGUSR1, function () use (&$invoked) {
                     $invoked = true;
                 });
                 $loop->unreference($watcher);
