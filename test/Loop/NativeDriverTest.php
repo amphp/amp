c4d172c226f6324e987d610f42949916912de486
Niklas Keller <me@kelunik.com>
2021-01-10 16:42:46 +0100

    Fix NativeDriverTest if there's a slightly different max FD

--- test/Loop/NativeDriverTest.php
+++ test/Loop/NativeDriverTest.php
@@ -29,7 +29,7 @@ class NativeDriverTest extends DriverTest
         }
 
         $this->expectException(\Exception::class);
-        $this->expectExceptionMessage("You have reached the limits of stream_select(). It has a FD_SETSIZE of 1024, but you have file descriptors numbered at least as high as 2006. You can install one of the extensions listed on https://amphp.org/amp/event-loop/#implementations to support a higher number of concurrent file descriptors. If a large number of open file descriptors is unexpected, you might be leaking file descriptors that aren't closed correctly.");
+        $this->expectExceptionMessage("You have reached the limits of stream_select(). It has a FD_SETSIZE of 1024, but you have file descriptors numbered at least as high as 200");
 
         $this->start(function (Driver $loop) use ($sockets) {
             $loop->delay(100, function () {
