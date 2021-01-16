fba646935481de4dcb5b495a722a7ad94faf63ef
Niklas Keller <me@kelunik.com>
2021-01-16 16:16:15 +0100

    Skip PsalmTest if psalm is not installed

--- test/PsalmTest.php
+++ test/PsalmTest.php
@@ -15,6 +15,10 @@ class PsalmTest extends TestCase
             self::markTestSkipped('Skipped on Windows');
         }
 
+        if (!\file_exists('vendor/bin/psalm.phar')) {
+            self::markTestSkipped('Missing psalm.phar');
+        }
+
         $issues = \json_decode(
             \shell_exec('./vendor/bin/psalm.phar --output-format=json --no-progress --config=psalm.examples.xml'),
             true
@@ -26,17 +30,17 @@ class PsalmTest extends TestCase
 
             if (!\preg_match('(// psalm-expect (.*))', $fileLines[$issue['line_from'] - 2] ?? '', $match)) {
                 self::fail('Psalm reports an issue that isn\'t marked as expected: ' . \json_encode(
-                    $issue,
-                    \JSON_PRETTY_PRINT
-                ));
+                        $issue,
+                        \JSON_PRETTY_PRINT
+                    ));
             }
 
             $expectedIssues = \array_map('trim', \explode(',', $match[1]));
             if (!\in_array($issue['type'], $expectedIssues, true)) {
                 self::fail('Psalm reports an issue that isn\'t marked as expected: ' . \json_encode(
-                    $issue,
-                    \JSON_PRETTY_PRINT
-                ));
+                        $issue,
+                        \JSON_PRETTY_PRINT
+                    ));
             }
         }
 
