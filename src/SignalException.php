d4fe7a9b9986a1bead1fe0cb2113a46b9e657091
Aaron Piotrowski <aaron@trowski.com>
2022-05-25 22:42:13 -0500

    Add SignalCancellation

---
+++ src/SignalException.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Amp;
+
+/**
+ * Used as the previous exception to {@see CancelledException} when a {@see SignalCancellation} is triggered.
+ *
+ * @see SignalCancellation
+ */
+class SignalException extends \Exception
+{
+    /**
+     * @param string $message Exception message.
+     */
+    public function __construct(string $message = "Operation cancelled by signal")
+    {
+        parent::__construct($message);
+    }
+}
---
+++ test/Cancellation/SignalCancellationTest.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Amp\Cancellation;
+
+use Amp\CancelledException;
+use Amp\PHPUnit\AsyncTestCase;
+use Amp\SignalCancellation;
+use Amp\SignalException;
+use Revolt\EventLoop;
+use function Amp\delay;
+
+/**
+ * @requires ext-pcntl
+ * @requires ext-posix
+ */
+class SignalCancellationTest extends AsyncTestCase
+{
+    public function testSignal(): void
+    {
+        $line = __LINE__ + 1;
+        $cancellation = new SignalCancellation([\SIGUSR1, \SIGUSR2]);
+
+        self::assertFalse($cancellation->isRequested());
+
+        EventLoop::defer(function (): void {
+            \posix_kill(\getmypid(), \SIGUSR1);
+        });
+
+        delay(0.1);
+
+        self::assertTrue($cancellation->isRequested());
+
+        try {
+            $cancellation->throwIfRequested();
+        } catch (CancelledException $exception) {
+            self::assertInstanceOf(SignalException::class, $exception->getPrevious());
+
+            $message = $exception->getPrevious()->getMessage();
+
+            if ((int) \ini_get('zend.assertions') > 0) {
+                self::assertStringContainsString('SignalCancellation was created here', $message);
+                self::assertStringContainsString('SignalCancellationTest.php:' . $line, $message);
+            }
+        }
+    }
+
+    public function testWatcherCancellation(): void
+    {
+        $enabled = EventLoop::getInfo()["on_signal"]["enabled"];
+        $cancellation = new SignalCancellation([\SIGUSR1, \SIGUSR2]);
+        self::assertSame($enabled + 2, EventLoop::getInfo()["on_signal"]["enabled"]);
+        unset($cancellation);
+        self::assertSame($enabled, EventLoop::getInfo()["on_signal"]["enabled"]);
+    }
+}
