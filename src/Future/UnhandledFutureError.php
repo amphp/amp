88b42d1d4d6f04f41cd75e6c151e7d701f5a00d7
Niklas Keller <me@kelunik.com>
2022-10-24 21:53:54 +0200

    Track origin of unhandled futures

--- src/Future/UnhandledFutureError.php
+++ src/Future/UnhandledFutureError.php
@@ -7,11 +7,15 @@ namespace Amp\Future;
  */
 final class UnhandledFutureError extends \Error
 {
-    public function __construct(\Throwable $previous)
+    public function __construct(\Throwable $previous, ?string $origin = null)
     {
         $message = 'Unhandled future: ' . $previous::class . ': "' . $previous->getMessage()
             . '"; Await the Future with Future::await() before the future is destroyed or use '
-            . 'Future::ignore() to suppress this exception';
+            . 'Future::ignore() to suppress this exception.';
+
+        if ($origin) {
+            $message .= 'The future has been created at ' . $origin;
+        }
 
         parent::__construct($message, 0, $previous);
     }
